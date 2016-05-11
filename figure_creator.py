829b3de0774cadc0a22fdfb8b8c036f6e24891ca
mcm7f <mcm7f@mtmail.mtsu.edu>
2016-05-11 14:21:41 -0500

    updated figure_creator to generate plots of uniform and gaussian noise examples

--- figure_creator.py
+++ figure_creator.py
@@ -4,7 +4,10 @@
 # NOTE: This program requires an aggregated log file as a command line argument
 #       This program will parse the aggregated log file and produce the
 #       following figures based off the following options:
-#       -v    fractal dimension/noise and slope variance/noise
+#       -v  aggregate.file  fractal dimension/noise and slope variance/noise
+#       -u  n1 n2 image_name    Uniform:  Small Image | Noisey Image | Noisier Image
+
+
 #       -a    fractal dimension/noise versus acceleration/noise
 #       -o    fractal dimension/noise versus other/noise 
 #       
@@ -21,18 +24,27 @@ import matplotlib.image as mpimg
 from math import log
 from statistics import variance
 import time
+from random import randint
+from scipy import ndimage
+
+PREVIEW = False
 
 def main():
     print( "Detected", len(sys.argv), "arguments")
 
-    # Parse an aggregated log file
-    filename = sys.argv[1]
-    file = open( filename, 'r' )
-    image_name = filename.split('/')
-    image_name = image_name[-1].split('_')
-    image_name = image_name[0]
+    if len(sys.argv) < 3:
+        print("Insufficient Arguments")
+        return
 
-    if len(sys.argv) > 2 and sys.argv[2] == "-v":
+    # expect a '-xyz' type of argument
+    if sys.argv[1][1] == "v":
+        # Parse an aggregated log file
+        filename = sys.argv[2]
+        file = open( filename, 'r' )
+        image_name = filename.split('/')
+        image_name = image_name[-1].split('_')
+        image_name = image_name[0]
+        
         # Preparing to plot Dimension vs. Uniform Noise and Slope Variance vs. Uniform Noise
         d = []
         sv = []
@@ -54,7 +66,7 @@ def main():
         # Now prepare to create the figure
         fig, axarr = plt.subplots(2, sharex=True)
         fig.suptitle("Box Count Algorithm on " + image_name + " (Uniform Noise, 100 Seeds)", fontsize=14, fontweight='bold')
-        fig.set_size_inches(12,12,forward=True)  # try to set size of plot??
+        fig.set_size_inches(10,10,forward=True)  # try to set size of plot??
 
         # set up gridlines for 1st plot
         axarr[0].grid(b=True, which='major', color=AXIS_GRAY, linestyle='-')
@@ -69,7 +81,7 @@ def main():
         # Plot a horizontal bar at initial dimension value
         axarr[0].plot((un[1],100),(horiz_bar,horiz_bar),'g--')
         # Annotate some text highlighting original dimension
-        axarr[0].text(.001, d[0]+0.02, "Dimension = " + str(d[0]), color='g')
+        axarr[0].text(.001, d[0]+0.02, "Dimension = " + format(d[0], '.2f'), color='g')
         axarr[0].plot(un, d, label="dimension")
         
         # set up gridlines for 2nd plot
@@ -88,230 +100,219 @@ def main():
         axarr[1].plot(un,sv, label="variance")
 
         plt.savefig("Figure_DvsSV_Uniform_"+image_name+".png")
+        if( PREVIEW ):
+            plt.show()
+    # ================================================================================
+    # Uniform Noise Examples
+    # python3 figure_creator.py -u 1 10 50 imagename.png
+    # ================================================================================# Uniform Noise Examples
+    elif sys.argv[1][1] == "u":
+        if( len(sys.argv) != 6 ):
+            print("-u not enough arguments")
+            return
+        n1 = float(sys.argv[2])
+        n2 = float(sys.argv[3])
+        n3 = float(sys.argv[4])
+        image = sys.argv[5]
+        
+        print("@=====================@")
+        print("| Uniform Figure Plot")
+        print("| Image:", image)
+        print("@=====================@")
+        
+        image_name = image.split('/')
+        image_name = image_name[-1].split('_')
+        image_name = image_name[0]
+
+        img = get_uniform_rgb_from(image)
+        
+        # Make a figure that holds multiple plots
+        fig = plt.figure()
+        fig.set_size_inches(12,12,forward=True)  # try to set size of plot??
+        fig.suptitle("Uniform Noise Added to " + image_name, fontsize=14, fontweight='bold')
+
+        # first plot is image with no noise added
+        a = fig.add_subplot(2,2,1)  # 2 rows, 2 cols, 1st image?
+        imgplot = plt.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title('Original Image')
+
+        a = fig.add_subplot(2,2,2) # second plot has n1 noise added
+        newimg = addUniformNoise( img, n1, 10 ) # using SEED = 10 ... it's just an example figure
+        imgplot = plt.imshow(newimg, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title("Added Noise at " + str(n1) + "%")
+
+        a = fig.add_subplot(2,2,3) # second plot has n1 noise added
+        newimg = addUniformNoise( img, n2, 10 ) # using SEED = 10 ... it's just an example figure
+        imgplot = plt.imshow(newimg, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title("Added Noise at " + str(n2) + "%")
+
+        a = fig.add_subplot(2,2,4) # second plot has n1 noise added
+        newimg = addUniformNoise( img, n3, 10 ) # using SEED = 10 ... it's just an example figure
+        imgplot = plt.imshow(newimg, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title("Added Noise at " + str(n3) + "%")
+
+        plt.savefig("Figure_Uniform_"+image_name)
+        print("Figure_Uniform_"+image_name)
         plt.show()
+        
+    # ================================================================================
+    # Gaussian Noise Examples
+    # python3 figure_creator.py -g 10 100 imagename.png
+    # ================================================================================
+    elif sys.argv[1][1] == "g":
+        if( len(sys.argv) != 5 ):
+            print("-g not enough arguments")
+            return
+        sig1 = float(sys.argv[2])  # first example case
+        sig2 = float(sys.argv[3])  # second example case
+        image = sys.argv[4]
+        print("@=====================@")
+        print("| Gaussian Figure Plot")
+        print("| Image:", image)
+        print("@=====================@")
+
+        img = get_gaussian_rgb_from(image)
+        
+        # Make a figure that holds multiple plots
+        fig = plt.figure()
+        fig.set_size_inches(12,12,forward=True)  # try to set size of plot??
+        a = fig.add_subplot(2,2,1)  # 2 rows, 2 cols, 1st image?
+
+        # first plot - raw image   
+        ##imgplot = plt.imshow(img, cmap=plt.cm.gray, interpolation='bicubic')
+        ##a.set_title('Initial Image')
+
+        ##a = fig.add_subplot(1,3,2)    # 1 row, 3 cols, 2nd image?
+
+        # try applying a gaussian filter...
+        img = ndimage.gaussian_filter(img, sigma=(sig1), order=0)
+        #print(img[40])
+        imgplot = plt.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title('Gaussian Sigma=' + str(sig1))
+
+        # Now I need to reduce the image to a single floating point value....
+
+        imgFiltered = np.empty( [len(img),len(img[0])],dtype=int )  # temp matrix - all 0 ints - same size as image
+        for row in range( len(imgFiltered) ):
+            for col in range( len(imgFiltered[0]) ):
+                # Gaussian filter algorithm here:
+                if( np.random.random() > img[row][col][0] ):
+                    imgFiltered[row][col] = 0   # set to black
+                else:
+                    imgFiltered[row][col] = 1
+
+        a = fig.add_subplot(2,2,2)   # 3rd image (was (1,3,3)
+        #print(imgFiltered[40])
+        imgplot = plt.imshow(imgFiltered, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title('Converted to B/W')
+
+
+        a = fig.add_subplot(2,2,3)  # 2 rows, 2 cols, 1st image?
+
+        # first plot - raw image   
+        ##imgplot = plt.imshow(img, cmap=plt.cm.gray, interpolation='bicubic')
+        ##a.set_title('Initial Image')
+
+        ##a = fig.add_subplot(1,3,2)    # 1 row, 3 cols, 2nd image?
+
+        # try applying a gaussian filter...
+        img = ndimage.gaussian_filter(img, sigma=(sig2), order=0)
+        #print(img[40])
+        imgplot = plt.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title('Gaussian Sigma=' + str(sig2))
+
+        # Now I need to reduce the image to a single floating point value....
+
+        imgFiltered = np.empty( [len(img),len(img[0])],dtype=int )  # temp matrix - all 0 ints - same size as image
+        for row in range( len(imgFiltered) ):
+            for col in range( len(imgFiltered[0]) ):
+                # Gaussian filter algorithm here:
+                if( np.random.random() > img[row][col][0] ):
+                    imgFiltered[row][col] = 0   # set to black
+                else:
+                    imgFiltered[row][col] = 1
+
+        a = fig.add_subplot(2,2,4)   # 3rd image (was (1,3,3)
+        #print(imgFiltered[40])
+        imgplot = plt.imshow(imgFiltered, cmap=plt.cm.gray, interpolation='nearest')
+        a.set_title('Converted to B/W')
 
         
+        
+        image_name = image.split('/')
+        image_name = image_name[-1].split('_')
+        image_name = image_name[0]
+        fig.suptitle("Gaussian Noise Added to " + image_name, fontsize=14, fontweight='bold')
+        plt.savefig("Figure_Gaussian_"+image_name)
+        print("Figure_Gaussian_"+image_name)
+        plt.show()
+
+    else:
+        print("Incorrect arguments.  See figure_creator.py header")
+        sig1 = float(sys.argv[2])
+        
+        
+
 
     return
 
 
+# Small helper function: imports the png image file,
+# strips the alpha channel, and returns a numpy array
+# containing the red-green-blue channel for each pixel
+def get_gaussian_rgb_from(filename):
+    img3D = mpimg.imread(filename, )            # read png img file
+    img3D = np.delete(img3D,np.s_[3:],axis=2)   # strips alpha channel
 
+    return img3D  # was img
 
-    
-
-    # Here is the array of box widths
-    d = [3, 5, 10, 20, 30, 50, 100, 150, 300]
-
-    F = {}  # Data Dictionary using filenames as keys
-    filelist = sys.argv[1:]  # hold a list of files used
-    for filename in filelist:
-        N = {} # Data Dictonary holding Noise data
-        file = open(filename, 'r')
-        for line in file:
-            if line[0] != '#':
-                line = line.strip()
-                data = line.split(',')
-                # Noise % is the key, the array of counted boxes is the data
-                N[ data[1] ] = data[4:13]
-        F[filename] = N
-
-    # Steal image file name from the path:
-    image_name = sys.argv[1].split("/")
-    image_name = image_name[-1].split("_")
-    image_name = image_name[0]
-
-    # Debug print the size of our data structure
-    print(">> Size of F:",len(F))
-
-    # Now that we've read in the data, it is time to process it.
-    R = {}  # Data Dictionary holding aggregate results
-    for filekey in F:
-        for noisekey in F[filekey]:
-            # F[filekey][noisekey] holds one array of 9 boxcounts
-            # Need to calculate an arithmetic mean of the dimension (dim_mean)
-            
-            log_bc = []
-            # store the log(boxcounts)
-            for bc in F[filekey][noisekey]:
-                bc = int(bc)
-                if( bc == 0 ):
-                    log_bc.append( log(1) )
-                else:
-                    log_bc.append( log(bc) )
-
-            slopes = []
-            # Calculate Dimension slopes by taking delta(log(boxcounts) / delta(log(1/d))
-            for i in range(len(d)-1):
-                slopes.append( (log_bc[i] - log_bc[i+1]) / (log(1/d[i]) - log(1/d[i+1])) )
-
-            dim_mean = np.mean(slopes)
-            
-            # Need to calculate the variance of the dimensional slopes (dsl_var)
-            dsl_var = np.var(slopes, ddof=1)   #ddof = 1 matches old-style variance calculations
-
-            # Add each dim/var calculation to an array and store in results.
-            if noisekey in R:
-                R[noisekey].append( (dim_mean,dsl_var) )
+# Small helper function: imports the png image file,
+# strips the alpha channel, and returns a numpy array
+# containing the red-green-blue channel for each pixel
+def get_uniform_rgb_from(filename):
+    img3D = mpimg.imread(filename, )            # read png img file
+    img3D = np.delete(img3D,np.s_[3:],axis=2)   # strips alpha channel
+    # condense into true 2D array
+    img = np.empty( [len(img3D),len(img3D[0])],dtype=int )
+    for row in range( len(img) ):
+        for col in range( len(img[0]) ):
+            if img3D[row][col].sum() == 0:
+                img[row][col] = 0
             else:
-                R[noisekey] = [ (dim_mean,dsl_var) ]
+                img[row][col] = 1
+    return img
 
-    # Now all the results should be processed;  currently R is an array of tuples (Dim, Var)
-    print("Size of R:", len(R), "# results:",len(R['0.2']))
-    #print(">>",R['0.2'])
+# create a Uniform noise model with percent as parameter
+# Make sure to seed the noise!
+def addUniformNoise(inputArray, percent, seed):
+    # Need to calculate the threshold of the noise added based off of size of input Array
+    # i.e. if input array is 2100 x 1800 pixels, and percent = 1%,
+    # Then we would calculate: 2100x1800*0.01 = 37800 as threshold
+    # Minimum number = 0, Maximum number = 2100x1800
 
-    # separate the values and run statistics on them.
-    aggregated_results = []   # store final data to print here
-    for noisekey in R:
-        dim_array = []
-        dsl_array = []
-        # separate the data
-        for dataset in R[noisekey]:  # dataset is 100 pairs of (Dim, Var)
-            dim_array.append(dataset[0])
-            dsl_array.append(dataset[1])
-        
-        # calculate statistics
-        average_dimensional_estimate = np.mean(dim_array)
-        average_slope_variance_estimate = np.mean(dsl_array)
-        dim_standard_error = np.std(dim_array,ddof=1)/len(R[noisekey])
-        dsl_standard_error = np.std(dsl_array,ddof=1)/len(R[noisekey])
-        # add to aggregated results
-        aggregated_results.append( (float(noisekey), average_dimensional_estimate, dim_standard_error, average_slope_variance_estimate, dsl_standard_error) )
-
-    aggregated_results.sort()
+    # apply the seed
+    np.random.seed(seed)
     
-    #for item in aggregated_results:
-    #    print(">> ", item)
-        
-    # TODO: Need to save this data to a file
-    aggregate_datalog( aggregated_results, filelist, image_name )
-        
-    # Attempt to plot:
-    nx = []
-    dimy = []
-    vary = []
-    dimerr = []
-    varerr = []
-    for item in aggregated_results:
-        nx.append( item[0] )
-        dimy.append( item[1] )
-        dimerr.append( item[2] )
-        vary.append( item[3] )
-        varerr.append( item[4] )
+    # create uniform matrix between 0 and 1
+    noise = np.random.uniform(size=(len(inputArray),len(inputArray[0])))
 
-    fig, axarr = plt.subplots(2, sharex=True)
-    fig.suptitle("Box Count Algorithm on " + image_name + " (Uniform Noise, 100 Seeds)", fontsize=14, fontweight='bold')
-    axarr[0].set_ylabel("dimension")
-    axarr[0].set_xscale('log')
-    axarr[0].set_title("Mean Fractal Dimension vs. Noise %")
-    axarr[0].set_ylim(0,2)
-    axarr[0].errorbar(nx,dimy,dimerr,fmt='r')
-    axarr[0].plot(nx,dimy, label="dimension")
+    # only select percentage value of numbers as valid noise
+    threshold = percent / 100
+    noise = np.where( noise < threshold, -1, 0 )   # -1 signals probabilty of noise
 
-    axarr[1].set_ylabel("slope variance")
-    axarr[1].set_xlabel("noise %")
-    axarr[1].set_xscale('log')
-    axarr[1].set_title("Mean Slope Variance vs. Noise %")
-    axarr[1].set_ylim(0,1)
-    axarr[1].errorbar(nx,vary,varerr,fmt='r')
-    axarr[1].plot(nx,vary, label="variance")
+    # where inputArray is 0 (black), flip noise sign to + to attenuate signal
+    noise = np.where( inputArray < 1, noise*-1, noise )
 
-    plt.savefig("figures/" + image_name + "_uniform_D_V_vs_N.png")
-    #plt.show()
-        
+    # now add noise to image
+    inputArray = inputArray + noise
 
-# results_array - an array of tuples of the form (noise, dimension, dimension std err, variance, variance std err)
-# filelist - a list of filenames that were included in the aggregated data
-# Version: V1.00 - initial log format
-def aggregate_datalog( results_array, filelist, image_name ):
-    LOG_VERSION = "1.00"
-    pathname = "logfiles/"
-    filename = pathname + image_name + "_" + str(len(filelist)) + "_aggregateLog.csv"
-
-    log = open(filename, 'w')
-    # Write the header
-    log.write("# Aggregate Log Version: " + LOG_VERSION + ",,,,\n")
-    log.write("# Log Name: " + filename + ",,,,\n")
-    log.write("# Noise%, Dimension, Dim Std Err, Variance, Var Std Err\n")
-
-    # Write the data
-    for data in results_array:
-        log.write(str(data[0]) + "," + str(data[1]) + "," + str(data[2]) + "," + str(data[3]) + "," + str(data[4]) + "\n")
-
-    # Write a list of the files included in this data aggregation
-    log.write("# The data files below are included in this aggregation:,,,,\n")
-    for file in filelist:
-        log.write("# " + file + ",,,,\n")
-
-    print(">> Created aggregate datalog:", filename)
-    log.close()
-
-    
-    
-    
-
-
-
-
-
-
+    ''' old noise -- only additive 
+    inputArray = inputArray + noise
+    inputArray = np.where( inputArray > 0, 1, 0 )
+    '''
+    return inputArray
 
 main()
-'''
-
-# example data
-#x = np.arange(0, 10, 1)
-noise = np.array([0.0001, 0.001, 0.01, 0.1, 1, 10, 50])
-dim = np.array([1.1, 1.15, 1.3, 1.6, 1.85, 1.95, 2])
-var = np.array([0.1, 0.3, 0.2, 0.1, 0.01, 0.001, 0])
-
-
-
-# example variable error bar values
-stderr = np.array([.01,.1,.1,.05,.02,.01,0])
-#xerr = 0.1 + yerr
-
-# First illustrate basic byplot interface, using defaults where possible
-#fig = plt.figure()  # what does this do?
-#fig.suptitle("Main Title", fontsize=14, fontweight='bold')
-
-
-fig, axarr = plt.subplots(2, sharex=True)
-fig.suptitle("Dimension and Slope Variance vs. Noise %", fontsize=14, fontweight='bold')
-axarr[0].set_ylabel("dimension")
-axarr[0].set_xscale('log')
-axarr[0].errorbar(noise,dim,stderr,fmt='ro')
-axarr[0].plot(noise,dim, label="dimension")
-
-axarr[1].set_ylabel("slope variance")
-axarr[1].set_xlabel("noise %")
-axarr[1].set_xscale('log')
-axarr[1].errorbar(noise,var,stderr,fmt='ro')
-axarr[1].plot(noise,var, label="variance")
-
-'''
-
-'''
-# create subplot (so that axis can be manipulated
-ax = fig.add_subplot(111)
-ax.set_title("subtitle")
-ax.set_xlabel("noise axis")
-ax.set_ylabel("dimension")
-ax.set_xscale('log')            # set x-axis to log scale
-
-#plt.errorbar(x, dim, yerr=yerr, fmt='ro')  # error bar plot, formatting
-ax.errorbar(noise,dim, stderr, fmt='ro')
-ax.errorbar(noise,var, stderr, fmt='ro')
-ax.plot(noise,dim)                          # normal plot, formatting
-ax.plot(noise,var)
-#plt.title("Simplest errorbars, 0.2 in x, 0.4 in y")
-'''
-
-'''
-plt.savefig("thesisPlotsExcample.png")
-
-plt.show()
-'''
-
 
 
 
