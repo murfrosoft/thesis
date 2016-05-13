7a9dac43f9d39fb47e5d69e3369b39306cca5367
mcm7f <mcm7f@mtmail.mtsu.edu>
2016-05-13 06:55:13 -0500

    aggregator function now creates multiple keys

--- aggregator.py
+++ aggregator.py
@@ -43,7 +43,7 @@ def main():
                 N[ data[1] ] = data[4:13]
         F[filename] = N
 
-    # Steal image file name from the path:
+    # Grab image file name from the path:
     image_name = sys.argv[2].split("/")
     image_name = image_name[-1].split("_")
     image_name = image_name[0]
@@ -72,16 +72,37 @@ def main():
             for i in range(len(d)-1):
                 slopes.append( (log_bc[i] - log_bc[i+1]) / (log(1/d[i]) - log(1/d[i+1])) )
 
+            # FRACTAL DIMENSION = average of slope estimates
             dim_mean = np.mean(slopes)
             
-            # Need to calculate the variance of the dimensional slopes (dsl_var)
+            # KEY1: Slope Variance -- Need to calculate the variance of the dimensional slopes (dsl_var)
             dsl_var = np.var(slopes, ddof=1)   #ddof = 1 matches old-style variance calculations
+
+            # KEY2: Sum( Abs( Diff ) ) SAD
+            SAD = 0
+            for i in range(len(slopes) - 1):
+                SAD += np.fabs( slopes[i]-slopes[i+1] )
+
+            # KEY3: Sum( Abs( Central Difference ) ) SAC
+            CentralDiffs = []
+            for i in range(len(slopes) - 1):
+                CentralDiffs.append( (slopes[i]+slopes[i+1])/2 )
+
+            SAC = 0
+            for cd in CentralDiffs:
+                SAC += np.fabs( cd )
+
+            # KEY4: Avg Difference from mean squared - ADMS
+            DMSsums = 0
+            for slope in slopes:
+                DMSsums += (slope - dim_mean)**2
+            ADMS = DMSsums / len(slopes)              
 
             # Add each dim/var calculation to an array and store in results.
             if noisekey in R:
-                R[noisekey].append( (dim_mean,dsl_var) )
+                R[noisekey].append( (dim_mean, dsl_var, SAD, SAC, ADMS) )
             else:
-                R[noisekey] = [ (dim_mean,dsl_var) ]
+                R[noisekey] = [ (dim_mean, dsl_var, SAD, SAC, ADMS) ]
 
     # Now all the results should be processed;  currently R is an array of tuples (Dim, Var)
     print("Size of R:", len(R)) #, "# results:",len(R['0.2']))
@@ -92,18 +113,30 @@ def main():
     for noisekey in R:
         dim_array = []
         dsl_array = []
+        SAD_array = []
+        SAC_array = []
+        ADMS_array = []
         # separate the data
         for dataset in R[noisekey]:  # dataset is 100 pairs of (Dim, Var)
             dim_array.append(dataset[0])
             dsl_array.append(dataset[1])
+            SAD_array.append(dataset[2])
+            SAC_array.append(dataset[3])
+            ADMS_array.append(dataset[4])
         
         # calculate statistics
-        average_dimensional_estimate = np.mean(dim_array)
-        average_slope_variance_estimate = np.mean(dsl_array)
-        dim_standard_error = np.std(dim_array,ddof=1)/len(R[noisekey])
-        dsl_standard_error = np.std(dsl_array,ddof=1)/len(R[noisekey])
+        avg_dim = np.mean(dim_array)
+        avg_var = np.mean(dsl_array)
+        avg_sad = np.mean(SAD_array)
+        avg_sac = np.mean(SAC_array)
+        avg_adms = np.mean(ADMS_array)
+        dim_ste = np.std(dim_array,ddof=1)/len(R[noisekey])
+        var_ste = np.std(dsl_array,ddof=1)/len(R[noisekey])
+        sad_ste = np.std(SAD_array,ddof=1)/len(R[noisekey])
+        sac_ste = np.std(SAC_array,ddof=1)/len(R[noisekey])
+        adms_ste = np.std(ADMS_array,ddof=1)/len(R[noisekey])
         # add to aggregated results
-        aggregated_results.append( (float(noisekey), average_dimensional_estimate, dim_standard_error, average_slope_variance_estimate, dsl_standard_error) )
+        aggregated_results.append( (float(noisekey), avg_dim,dim_ste, avg_var,var_ste, avg_sad,sad_ste, avg_sac,sac_ste, avg_adms,adms_ste ) )
 
     aggregated_results.sort()
     
@@ -112,25 +145,45 @@ def main():
         
     # TODO: Need to save this data to a file
     aggregate_datalog( aggregated_results, filelist, image_name , noise_type)
-        
+
+    # Plot aggregated data
+    plot_agg( aggregated_results, image_name, len(F), noise_type)
+
+
+def plot_agg( aggregated_results, image_name, seed_count, noise_type):
     # Attempt to plot:
     nx = []
     dimy = []
     vary = []
+    sady = []
+    sacy = []
+    admsy = []
     dimerr = []
     varerr = []
+    saderr = []
+    sacerr = []
+    admserr = []
     for item in aggregated_results:
         nx.append( item[0] )
         dimy.append( item[1] )
         dimerr.append( item[2] )
         vary.append( item[3] )
         varerr.append( item[4] )
+        sady.append( item[5] )
+        saderr.append( item[6] )
+        sacy.append( item[7] )
+        sacerr.append( item[8] )
+        admsy.append( item[9] )
+        admserr.append( item[10] )
 
-    fig, axarr = plt.subplots(2, sharex=True)
+    # Create a figure with 5 subplots
+    fig, axarr = plt.subplots(5, sharex=True)
     if( noise_type == 'u'):
-        fig.suptitle("Box Count Algorithm on " + image_name + " (Uniform Noise, " + str(len(F)) + " Seeds)", fontsize=14, fontweight='bold')
+        fig.suptitle("Box Count Algorithm on " + image_name + " (Uniform Noise, " + str(seed_count) + " Seeds)", fontsize=14, fontweight='bold')
     else:
-        fig.suptitle("Box Count Algorithm on " + image_name + " (Gaussian Noise, " + str(len(F)) + " Seeds)", fontsize=14, fontweight='bold')
+        fig.suptitle("Box Count Algorithm on " + image_name + " (Gaussian Noise, " + str(seed_count) + " Seeds)", fontsize=14, fontweight='bold')
+
+        
     axarr[0].set_ylabel("dimension")
     axarr[0].set_xscale('log')
     axarr[0].set_title("Mean Fractal Dimension vs. Noise %")
@@ -139,12 +192,36 @@ def main():
     axarr[0].plot(nx,dimy, label="dimension")
 
     axarr[1].set_ylabel("slope variance")
-    axarr[1].set_xlabel("noise %")
+    #axarr[1].set_xlabel("noise %")
     axarr[1].set_xscale('log')
     axarr[1].set_title("Mean Slope Variance vs. Noise %")
     axarr[1].set_ylim(0,1)
     axarr[1].errorbar(nx,vary,varerr,fmt='r')
     axarr[1].plot(nx,vary, label="variance")
+    
+    axarr[2].set_ylabel("avg difference mean squared")
+    #axarr[2].set_xlabel("noise %")
+    axarr[2].set_xscale('log')
+    axarr[2].set_title("Mean Avg Diff Mean Squared vs. Noise %")
+    axarr[2].set_ylim(0,1)
+    axarr[2].errorbar(nx,admsy,admserr,fmt='r')
+    axarr[2].plot(nx,admsy, label="adms")
+    
+    axarr[3].set_ylabel("sum(abs(diff))")
+    #axarr[3].set_xlabel("noise %")
+    axarr[3].set_xscale('log')
+    axarr[3].set_title("Mean Sum of Diffs vs. Noise %")
+    axarr[3].set_ylim(0,10)
+    axarr[3].errorbar(nx,sady,saderr,fmt='r')
+    axarr[3].plot(nx,sady, label="sad")
+
+    axarr[4].set_ylabel("sum(abs(central diff))")
+    axarr[4].set_xlabel("noise %")
+    axarr[4].set_xscale('log')
+    axarr[4].set_title("Mean Sum of Central Diffs vs. Noise %")
+    axarr[4].set_ylim(0,20)
+    axarr[4].errorbar(nx,sacy,sacerr,fmt='r')
+    axarr[4].plot(nx,sacy, label="sac")
     
     if( noise_type == 'u'):
         plt.savefig("figures/" + image_name + "_uniform_D_V_vs_N.png")
@@ -157,95 +234,33 @@ def main():
 # results_array - an array of tuples of the form (noise, dimension, dimension std err, variance, variance std err)
 # filelist - a list of filenames that were included in the aggregated data
 # Version: V1.00 - initial log format
+# Version: V1.01 - simpler naming convention and new keys added
 def aggregate_datalog( results_array, filelist, image_name, noise_type ):
-    LOG_VERSION = "1.00"
+    LOG_VERSION = "1.01"
     pathname = "logfiles/"
-    filename = pathname + image_name + "_" + noise_type + str(len(filelist)) + "_aggregateLog.csv"
+    filename = pathname + image_name + "_" + noise_type + str(len(filelist)) + "_aglog.csv"
 
     log = open(filename, 'w')
     # Write the header
-    log.write("# Aggregate Log Version: " + LOG_VERSION + ",,,,\n")
-    log.write("# Log Name: " + filename + ",,,,\n")
-    log.write("# Noise%, Dimension, Dim Std Err, Variance, Var Std Err\n")
+    log.write("# Aggregate Log Version: " + LOG_VERSION + ",,,,,,,,,,\n")
+    log.write("# Log Name: " + filename + ",,,,,,,,,,\n")
+    log.write("# Noise%, Dimension, Dim Std Err, Variance, Var Std Err, Sum(Abs(Diff), SAD Std Err, Sum(Abs(CenDiff)), SAC Std Err, Avg Diff Mean Squared, ADMS Ste\n")
 
     # Write the data
     for data in results_array:
-        log.write(str(data[0]) + "," + str(data[1]) + "," + str(data[2]) + "," + str(data[3]) + "," + str(data[4]) + "\n")
+        log.write(str(data[0]) + "," + str(data[1]) + "," + str(data[2]) + "," + str(data[3]) + "," + str(data[4]) + "," + str(data[5]) + "," + str(data[6]) + ",")
+        log.write(str(data[7]) + "," + str(data[8]) + "," + str(data[9]) + "," + str(data[10]) + "\n")
 
     # Write a list of the files included in this data aggregation
-    log.write("# The data files below are included in this aggregation:,,,,\n")
+    log.write("# The data files below are included in this aggregation:,,,,,,,,,,\n")
     for file in filelist:
-        log.write("# " + file + ",,,,\n")
+        log.write("# " + file + ",,,,,,,,,,\n")
 
     print(">> Created aggregate datalog:", filename)
     log.close()
 
-    
-    
-    
-
-
-
-
-
-
 
 main()
-'''
-
-# example data
-#x = np.arange(0, 10, 1)
-noise = np.array([0.0001, 0.001, 0.01, 0.1, 1, 10, 50])
-dim = np.array([1.1, 1.15, 1.3, 1.6, 1.85, 1.95, 2])
-var = np.array([0.1, 0.3, 0.2, 0.1, 0.01, 0.001, 0])
-
-
-
-# example variable error bar values
-stderr = np.array([.01,.1,.1,.05,.02,.01,0])
-#xerr = 0.1 + yerr
-
-# First illustrate basic byplot interface, using defaults where possible
-#fig = plt.figure()  # what does this do?
-#fig.suptitle("Main Title", fontsize=14, fontweight='bold')
-
-
-fig, axarr = plt.subplots(2, sharex=True)
-fig.suptitle("Dimension and Slope Variance vs. Noise %", fontsize=14, fontweight='bold')
-axarr[0].set_ylabel("dimension")
-axarr[0].set_xscale('log')
-axarr[0].errorbar(noise,dim,stderr,fmt='ro')
-axarr[0].plot(noise,dim, label="dimension")
-
-axarr[1].set_ylabel("slope variance")
-axarr[1].set_xlabel("noise %")
-axarr[1].set_xscale('log')
-axarr[1].errorbar(noise,var,stderr,fmt='ro')
-axarr[1].plot(noise,var, label="variance")
-
-'''
-
-'''
-# create subplot (so that axis can be manipulated
-ax = fig.add_subplot(111)
-ax.set_title("subtitle")
-ax.set_xlabel("noise axis")
-ax.set_ylabel("dimension")
-ax.set_xscale('log')            # set x-axis to log scale
-
-#plt.errorbar(x, dim, yerr=yerr, fmt='ro')  # error bar plot, formatting
-ax.errorbar(noise,dim, stderr, fmt='ro')
-ax.errorbar(noise,var, stderr, fmt='ro')
-ax.plot(noise,dim)                          # normal plot, formatting
-ax.plot(noise,var)
-#plt.title("Simplest errorbars, 0.2 in x, 0.4 in y")
-'''
-
-'''
-plt.savefig("thesisPlotsExcample.png")
-
-plt.show()
-'''
 
 
 
